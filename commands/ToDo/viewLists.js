024b42b542a04a2061fd57dcfe2b7efff24f74d3
Add00 <add@00.com>
2023-12-19 11:56:17 -0500

    Added a basic view

--- commands/ToDo/viewLists.js
+++ commands/ToDo/viewLists.js
@@ -1,24 +1,24 @@
 const Task = require("../../src/Task.js");
 const List = require("../../src/List.js");
+const db = require("../../src/db/db.js");
 
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("View-a-specific-to-do-list")
-    .setDescription("It helps you to view a To Do List.")
-    .addStringOption((option) =>
-      option
-        .setName("name")
-        .setDescription("The name of the list to edit.")
-        .setRequired(true)
-    ),
+    .setName("View-all-lists")
+    .setDescription("It helps you to view a To Do List."),
   async execute(interaction) {
-    const name = interaction.options.getString("name");
-    const listItems = []; // Define an empty array for the list items
-    // ! Retrieve the list from the database and edit it
-    const list = new List(name, listItems);
-    const listJson = list.toJson();
-    await interaction.reply(`List! ${listJson}`);
+    const list = List.fromJson(await db.load('list'));
+
+    const embed = new EmbedBuilder()
+      .setColor('Random')
+      .setDescription('Lists')
+      .setTimestamp(list.dueDate)
+      .addFields({name: 'name', value: list.name})
+      .addFields({name: 'owner', value: list.owner.username});
+    
+    await interaction.reply({ embeds: [embed], ephemeral: true })
+      .catch(console.error);
   },
 };
--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,11 @@
-const fs = require("node:fs");
+const fs = require('node:fs/promises');
 
 async function load(name) {
-    return fs.readFile(`./saves/${name}.json`, err => { console.warn(err); });
+    return fs.readFile(`./saves/${name}.json`, { encoding: 'utf8' });
 }
 
 async function save(name, json) {
-    fs.writeFile(`./saves/${name}.json`, json, err => { console.warn(err) });
+    await fs.writeFile(`./saves/${name}.json`, json);
 }
 
 module.exports = { load, save };
