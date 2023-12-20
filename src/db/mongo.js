2bb30c10c02ae00a6c279a9e8b6227b082774869
agam-singh-talwar <87700905+agam-singh-talwar@users.noreply.github.com>
2023-12-20 10:51:10 -0500

    Connected to mongo

---
+++ src/db/mongo.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@senecaweb.grvntlm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("A2_Bot");
+    // Create a new collection named "A2 Bot"
+    const collection = db.collection("Lists");
+
+    // Insert a document into the new collection
+    const result = await collection.insertOne({ test: "test" });
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+    // Close the database connection when finished or an error occurs
+    await client.close();
+  }
+}
+run().catch(console.error);
