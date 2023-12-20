64b8559776424abba08df2aa03549ab1c87a5ea9
agam-singh-talwar <87700905+agam-singh-talwar@users.noreply.github.com>
2023-12-20 11:22:37 -0500

    Now we r able to to cerate list in the db

--- src/db/db.js
+++ src/db/db.js
@@ -1,32 +1,22 @@
-const fs = require("fs/promises");
+// db.js
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@senecaweb.grvntlm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
 
-async function load(name) {
+async function connect() {
   try {
-    const data = await fs.readFile(`./saves/${name}.json`, "utf-8");
-    return data;
-  } catch (err) {
-    console.warn(err);
-  }
-}
-async function save(name, json) {
-  try {
-    // Read the existing file
-    const data = await fs.readFile(`./saves/${name}.json`, "utf-8");
-    var jsonData = JSON.parse(data);
-
-    // Append the new data
-    const newJsonData = JSON.parse(json);
-    if (Array.isArray(jsonData)) {
-      jsonData.push(newJsonData);
-    } else {
-      jsonData = [jsonData, newJsonData];
+    if (!(client.topology && client.topology.isConnected())) {
+      await client.connect();
     }
 
-    // Write the updated data back to the file
-    await fs.writeFile(`./saves/${name}.json`, JSON.stringify(jsonData));
+    const db = client.db("A2_Bot");
+    await db.command({ ping: 1 });
+    return client;
   } catch (err) {
-    console.warn(err);
+    console.error("Error connecting to the database", err);
+    throw err;
   }
 }
 
-module.exports = { load, save };
+module.exports = { connect };
